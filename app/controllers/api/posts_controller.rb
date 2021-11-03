feee1ee4dbd4e8c92d619d6e2c75a0b1c0ea0869
roger chat <mattrogerpelosi@gmail.com>
2021-11-03 15:52:33 -0400

    setting up api foler, namespaceing routes, creating sessions controller and setting up all controllers for api separation

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,5 @@
+class Api::PostsController < ApplicationController
+
+
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 Rails.application.routes.draw do
   
-  resources :comments
-  resources :posts
-  resources :users
-  # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
+  namespace :api do 
+
+    resources :comments
+
+    resources :posts
+
+    resources :users
+
+    get '/me', to: 'users#show'
+
+    post '/signup', to: 'users#create'
+
+    post '/login', to: 'sessions#create'
+
+    delete '/logout', to: 'sessions#destroy'
+
+  end
+
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
 end
