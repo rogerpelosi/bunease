0cd10d24e9b6beadc0c19d1c84b18df502992c4e
roger chat <mattrogerpelosi@gmail.com>
2021-11-04 11:41:49 -0400

    adding validations and secure password to user model, creating show/update/create actions tp users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,42 @@
 class Api::UsersController < ApplicationController
 
+    def create 
+        @new_user = User.new(user_params)
+        if @new_user.save
+            session[:user_id] = @new_user.id
+            render json: @new_user,
+            status: :created 
+        else 
+            render json: {error: @new_user.errors.full_messages}, 
+            status: :unprocessable_entity 
+        end 
+    end 
 
-    
+    def show 
+        if current_user
+            render json: current_user, 
+            status: :ok
+          else
+            render json: { error: 'No active session' }, 
+            status: :unauthorized
+          end
+    end 
+
+    def update 
+        current_user.update(user_params)
+        if current_user.save
+            render json: current_user,
+            status: :ok 
+        else 
+            render json: {error: current_user.errors.full_messages}, 
+            status: :unprocessable_entity 
+        end 
+    end 
+
+    private 
+
+    def user_params 
+        params.permit(:username, :name, :email, :bio, :profile_image, :password, :password_confirmation)
+    end 
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,9 @@ class User < ApplicationRecord
     has_many :posts
     has_many :chums, class_name: "User", foreign_key: "an_account_owner_id"
     #the above, chums, is going to be used in displaying a particular instances list of friends, or chums 
+
+    has_secure_password
+
+    validates :username, :email, uniqueness: true
     
 end
